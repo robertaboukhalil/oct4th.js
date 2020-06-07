f184f2b01e9097dfe7ca4b5a700a0473c237443a
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2020-06-07 15:17:06 -0700

    Fix header location

--- src/App.svelte
+++ src/App.svelte
@@ -94,17 +94,19 @@ function s2ab(s) {
 </script>
 
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-	<a class="navbar-brand" href="/">Oct4th</a>
-	<div class="collapse navbar-collapse">
-		<ul class="navbar-nav mr-auto"></ul>
-		<ul class="navbar-nav">
-			<li class="nav-item active">
-				<a class="nav-link" href="https://pypi.org/project/oct4th/" target="_blank">CLI Tool</a>
-			</li>
-			<li class="nav-item active">
-				<a class="nav-link" href="https://github.com/robertaboukhalil/oct4th.js" target="_blank">Code</a>
-			</li>
-		</ul>
+	<div class="container">
+		<a class="navbar-brand" href="/">Oct4th</a>
+		<div class="collapse navbar-collapse">
+			<ul class="navbar-nav mr-auto"></ul>
+			<ul class="navbar-nav">
+				<li class="nav-item active">
+					<a class="nav-link" href="https://pypi.org/project/oct4th/" target="_blank">CLI Tool</a>
+				</li>
+				<li class="nav-item active">
+					<a class="nav-link" href="https://github.com/robertaboukhalil/oct4th.js" target="_blank">Code</a>
+				</li>
+			</ul>
+		</div>
 	</div>
 </nav>
 
